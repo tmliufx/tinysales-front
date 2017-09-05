5aabb9ba7323fa597f080a71636b57e929d4357b
tmliufx <tmliufx@tinysales.net>
2017-09-05 21:40:42 +0800

    HashRouter不需要使用history属性

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,16 @@ import {
     HashRouter as Router,
     Route
 } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
 
 import TestView from '../views/test-view';
 import IndexView from '../views/index-view';
 import MainView from '../views/main-view';
 import LoginView from '../views/login-view';
 
-const history = createBrowserHistory()
-
 class AppRouter extends React.Component {
     render() {
         return (
-            <Router history={history}>
+            <Router>
                 <div>
                     <Route exact path="/" component={IndexView} />
                     <Route exact path="/login" component={LoginView} />
